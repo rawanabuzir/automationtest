5e361da7df2878ba6671c9fd5a6e7ab25c21f33b
Rawan_Abuzir <77068920+rawanabuzir@users.noreply.github.com>
2023-09-02 10:55:44 +0300

    Update second_test.js

--- projrct_num_two/cypress/e2e/second_test.js
+++ projrct_num_two/cypress/e2e/second_test.js
@@ -16,7 +16,7 @@ describe('Add four items to cart then do the checkout ', () => {
         cy.get('#block_frame_latest_1770 > .thumbnails > :nth-child(2) > .thumbnail > .pricetag > .productcart > .fa').click();
         cy.get('#block_frame_latest_1770 > .thumbnails > :nth-child(3) > .thumbnail > .pricetag > .productcart > .fa').click();
         cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(4) > .thumbnail > .pricetag > .productcart > .fa').click();
-        cy.get('.block_7 > .nav > .dropdown > .dropdown-toggle').click(); //to click on add to cart button
+        cy.get('.block_7 > .nav > .dropdown > .dropdown-toggle').click(); //to click on add to cart button or any where on the item card
         cy.get('#cart_checkout1').click(); // to click on the checkout button
         cy.get('#accountFrm > fieldset > .btn').click(); //click on continue button after choose the Register Account option from the Checkout Options card
 
