a672baa29bc42ad38950888c6a260e7e6b23a080
Rawan_Abuzir <77068920+rawanabuzir@users.noreply.github.com>
2023-09-02 10:54:22 +0300

    Update second_test.js

--- projrct_num_two/cypress/e2e/second_test.js
+++ projrct_num_two/cypress/e2e/second_test.js
@@ -10,15 +10,15 @@ describe('Add four items to cart then do the checkout ', () => {
     it('if checkout button is working correctlly', () => {
         cy.visit("https://www.automationteststore.com/");
         cy.login('rawan', 'rawan123456789')
-            //login or regester button existing on the header
+            //login or register button existing on the header
         cy.get(':nth-child(1) > .active').click();
         cy.get('#block_frame_latest_1770 > .thumbnails > :nth-child(1) > .thumbnail > .pricetag > .productcart > .fa').click();
         cy.get('#block_frame_latest_1770 > .thumbnails > :nth-child(2) > .thumbnail > .pricetag > .productcart > .fa').click();
         cy.get('#block_frame_latest_1770 > .thumbnails > :nth-child(3) > .thumbnail > .pricetag > .productcart > .fa').click();
         cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(4) > .thumbnail > .pricetag > .productcart > .fa').click();
-        cy.get('.block_7 > .nav > .dropdown > .dropdown-toggle').click(); //to click on add to chart button
+        cy.get('.block_7 > .nav > .dropdown > .dropdown-toggle').click(); //to click on add to cart button
         cy.get('#cart_checkout1').click(); // to click on the checkout button
-        cy.get('#accountFrm > fieldset > .btn').click(); //click on contenue button after choose the Register Account option from the Checkout Options card
+        cy.get('#accountFrm > fieldset > .btn').click(); //click on continue button after choose the Register Account option from the Checkout Options card
 
 
 
