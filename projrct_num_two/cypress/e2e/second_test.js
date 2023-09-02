ed35af6712bbf36bfe046f026559420bed90836d
Rawan_Abuzir <77068920+rawanabuzir@users.noreply.github.com>
2023-09-02 10:58:35 +0300

    Update second_test.js

--- projrct_num_two/cypress/e2e/second_test.js
+++ projrct_num_two/cypress/e2e/second_test.js
@@ -1,7 +1,6 @@
 /// <reference types= "cypress" />
 
 Cypress.Commands.add("login", (user, password) => {
-
     cy.get('#customer_menu_top > li > a').click(); //click on regester button
     cy.get('#loginFrm_loginname').type(user); // type the email or the user name
     cy.get('#loginFrm_password').type(password); // type the password
@@ -16,7 +15,7 @@ describe('Add four items to cart then do the checkout ', () => {
         cy.get('#block_frame_latest_1770 > .thumbnails > :nth-child(2) > .thumbnail > .pricetag > .productcart > .fa').click();
         cy.get('#block_frame_latest_1770 > .thumbnails > :nth-child(3) > .thumbnail > .pricetag > .productcart > .fa').click();
         cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(4) > .thumbnail > .pricetag > .productcart > .fa').click();
-        cy.get('.block_7 > .nav > .dropdown > .dropdown-toggle').click(); //to click on add to cart button or any where on the item card
+        cy.get('.block_7 > .nav > .dropdown > .dropdown-toggle').click(); //to click on add to cart button(byside the item name) or any where on the item card
         cy.get('#cart_checkout1').click(); // to click on the checkout button
         cy.get('#accountFrm > fieldset > .btn').click(); //click on continue button after choose the Register Account option from the Checkout Options card
 
